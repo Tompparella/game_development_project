e162bd02fa3d1b18bf0f941cb2c567fa6cbc1514
Tompparella <tommi.kunnari@hotmail.com>
2022-10-09 14:20:59 +0300

    Added clientside inter- and extrapolation for entity movement

--- client/Scenes/Singletons/GameManager.gd
+++ client/Scenes/Singletons/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const VIBE_TIMEOUT_TIME: float = 4.0
+const INTERPOLATION_OFFSET: float = 0.1
 
 var surroundings: Node2D
 var other_players: Node2D
@@ -8,9 +9,10 @@ var player: Player
 var camera: Camera
 var game_timer: Timer
 var placeholder_texture: String = "res://Assets/Items/Can.png"
+var player_template: PackedScene = preload("res://Scenes/Characters/PlayerTemplate.tscn")
 var returnables_on_map: int = 0
 var returnables_per_player: int = 75
-var player_template: PackedScene = preload("res://Scenes/Characters/PlayerTemplate.tscn")
+var world_state_buffer: Array = []
 # Run server sync only if GameManager has successfully initialized the game world on the client
 var initialized: bool = false
 
@@ -55,16 +57,14 @@ func _ready() -> void:
 	set_physics_process(false)
 
 func _physics_process(_delta) -> void:
-	# TODO: When player is freed, this will throw error. Connect player freeing to gamemanager to stop physics_process on player free
-	var player_state: Dictionary = {"p": player.global_position}
-	GameServer.UpdatePlayerState(player_state)
+	HandleWorldUpdate()
+	HandlePlayerUpdate()
 
 func Initialize() -> void:
-	player = get_node("../Game/Map/TileMap/Player")
+	EnablePlayer()
 	camera = get_node("../Game/Map/Camera")
 	surroundings = get_node("../Game/Map/TileMap/Surroundings")
 	other_players = get_node("../Game/Map/TileMap/OtherPlayers")
-	player.Initialize()
 	camera.Initialize(player)
 	UIControl.Initialize(player)
 	UIControl.HideLoginScreen()
@@ -72,6 +72,15 @@ func Initialize() -> void:
 	CreateGameTimer()
 	set_physics_process(true)
 	initialized = true
+
+func EnablePlayer() -> void:
+	if (has_node("../Game/Map/TileMap/Player")):
+		player = get_node("../Game/Map/TileMap/Player")
+		player.tree_exiting.connect(DisablePlayer)
+		player.Initialize()
+
+func DisablePlayer() -> void:
+	player = null
 
 func CreateGameTimer() -> void:
 	game_timer = Timer.new()
@@ -122,17 +131,47 @@ func _Returnable_Picked() -> void:
 
 func UpdateWorldState(world_state: Dictionary) -> void:
 	if initialized:
-		# We need to erase the timestamp property of the dictionary before iterating through player ids.
-		# It will be used for inter-and extrapolation.
-		world_state.erase("t")
-		# Erase this client from world state
-		world_state.erase(multiplayer.get_unique_id())
-		for player_id in world_state.keys():
-			if other_players.has_node(str(player_id)):
-				var entry: PlayerTemplate = other_players.get_node(str(player_id))
-				entry.MovePlayer(world_state[player_id]["p"])
-			else:
-				SpawnNewPlayer(player_id, world_state[player_id]["p"])
+		world_state_buffer.append(world_state)
+
+func HandleWorldUpdate() -> void:
+	# TODO: Handle player despawning
+	var render_time: float = Time.get_unix_time_from_system() - INTERPOLATION_OFFSET
+	if world_state_buffer.size() > 1:
+		while (world_state_buffer.size() > 2) && (render_time > world_state_buffer[2]["t"]):
+			world_state_buffer.remove_at(0)
+		var old_past: Dictionary = world_state_buffer[0]
+		var recent_past: Dictionary = world_state_buffer[1]
+		if world_state_buffer.size() > 2:
+			# Interpolation
+			var nearest_future: Dictionary = world_state_buffer[2]
+			var interpolation_factor = float(render_time - nearest_future["t"]) / float(nearest_future["t"] - recent_past["t"])
+			for player_id in nearest_future.keys():
+				# If the current entry is timestamp, the client's character, or if it doesn't exist in the past world state, continue
+				if (str(player_id) == "t" || player_id == multiplayer.get_unique_id() || !recent_past.has(player_id)):
+					continue
+				if other_players.has_node(str(player_id)):
+					var entry: PlayerTemplate = other_players.get_node(str(player_id))
+					var new_position: Vector2 = lerp(recent_past[player_id]["p"], nearest_future[player_id]["p"], interpolation_factor)
+					entry.MovePlayer(new_position)
+				else:
+					SpawnNewPlayer(player_id, nearest_future[player_id]["p"])
+		elif render_time > recent_past["t"]:
+			# Extrapolation
+			var extrapolation_factor: float = (float(render_time - old_past["t"]) / float(recent_past["t"] - old_past["t"])) - INTERPOLATION_OFFSET
+			for player_id in recent_past.keys():
+				# If the current entry is timestamp, the client's character, or if it doesn't exist in the past world state, continue
+				if (str(player_id) == "t" || player_id == multiplayer.get_unique_id() || !old_past.has(player_id)):
+					continue
+				if other_players.has_node(str(player_id)):
+					var entry: PlayerTemplate = other_players.get_node(str(player_id))
+					var position_delta: Vector2 = recent_past[player_id]["p"] - old_past[player_id]["p"]
+					var new_position: Vector2 = recent_past[player_id]["p"] + (position_delta * extrapolation_factor)
+					entry.MovePlayer(new_position)
+
+func HandlePlayerUpdate() -> void:
+	if player:
+		var player_state: Dictionary = {"p": player.global_position}
+		GameServer.UpdatePlayerState(player_state)
 
 func SpawnNewPlayer(player_id: int, spawn_location: Vector2) -> void:
 	# Check if the rpc call is for this player. If yes, do nothing
@@ -146,6 +185,9 @@ func SpawnNewPlayer(player_id: int, spawn_location: Vector2) -> void:
 	print("Player %s spawned" % str(player_id))
 
 func DespawnPlayer(player_id: int) -> void:
-	var despawned_player: Node = other_players.get_node(str(player_id))
-	if despawned_player:
+	if other_players.has_node(str(player_id)):
+		var despawned_player: Node = other_players.get_node(str(player_id))
+		# We need to await for a time in order to wait for the world state to empty its references to the despawned player.
+		# Without this, the player would despawn for a few milliseconds before being spawned again in the next world state update.
+		await get_tree().create_timer(0.2).timeout
 		despawned_player.queue_free()
